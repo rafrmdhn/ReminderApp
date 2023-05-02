446c241c7a370f9f14f143749d5b557d9031f284
Tania <89388944+nathaniasan@users.noreply.github.com>
2023-05-02 22:43:43 +0700

    Add files via upload

---
+++ projek_tam/app/src/main/java/com/example/projek_tam/all_tasks_page_activity.java
@@ -0,0 +1,92 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		all_tasks_page
+	 *	@date 		Tuesday 02nd of May 2023 01:51:20 AM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.projek_tam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class all_tasks_page_activity extends Activity {
+
+	
+	private View _bg__all_tasks_page_ek2;
+	private View rectangle_7;
+	private View ellipse_1;
+	private ImageView vector;
+	private ImageView vector_ek1;
+	private ImageView vector_ek2;
+	private TextView all_tasks;
+	private TextView today;
+	private ImageView vector_ek3;
+	private ImageView vector_ek4;
+	private ImageView vector_ek5;
+	private ImageView vector_ek6;
+	private ImageView vector_ek7;
+	private ImageView vector_ek8;
+	private ImageView vector_ek9;
+	private ImageView vector_ek10;
+	private TextView tomorrow;
+	private TextView upcomming;
+	private TextView someday;
+	private View rectangle_5;
+	private View rectangle_6;
+	private ImageView vector_ek11;
+	private TextView i_want____;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.all_tasks_page);
+
+		
+		_bg__all_tasks_page_ek2 = (View) findViewById(R.id._bg__all_tasks_page_ek2);
+		rectangle_7 = (View) findViewById(R.id.rectangle_7);
+		ellipse_1 = (View) findViewById(R.id.ellipse_1);
+		vector = (ImageView) findViewById(R.id.vector);
+		vector_ek1 = (ImageView) findViewById(R.id.vector_ek1);
+		vector_ek2 = (ImageView) findViewById(R.id.vector_ek2);
+		all_tasks = (TextView) findViewById(R.id.all_tasks);
+		today = (TextView) findViewById(R.id.today);
+		vector_ek3 = (ImageView) findViewById(R.id.vector_ek3);
+		vector_ek4 = (ImageView) findViewById(R.id.vector_ek4);
+		vector_ek5 = (ImageView) findViewById(R.id.vector_ek5);
+		vector_ek6 = (ImageView) findViewById(R.id.vector_ek6);
+		vector_ek7 = (ImageView) findViewById(R.id.vector_ek7);
+		vector_ek8 = (ImageView) findViewById(R.id.vector_ek8);
+		vector_ek9 = (ImageView) findViewById(R.id.vector_ek9);
+		vector_ek10 = (ImageView) findViewById(R.id.vector_ek10);
+		tomorrow = (TextView) findViewById(R.id.tomorrow);
+		upcomming = (TextView) findViewById(R.id.upcomming);
+		someday = (TextView) findViewById(R.id.someday);
+		rectangle_5 = (View) findViewById(R.id.rectangle_5);
+		rectangle_6 = (View) findViewById(R.id.rectangle_6);
+		vector_ek11 = (ImageView) findViewById(R.id.vector_ek11);
+		i_want____ = (TextView) findViewById(R.id.i_want____);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
